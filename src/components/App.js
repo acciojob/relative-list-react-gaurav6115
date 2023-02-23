aeb2a3a42f70657efbd189742c3e038ee7f40f24
gaurav6115 <gauravsaini602357@gmail.com>
2023-02-23 18:29:21 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+
+const INITIAL_STATE = ["Dinesh", "Ramesh", "Suresh"];
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key="relativeList" id="relativeList">
+        {INITIAL_STATE?.map((relative, idx) => {
+          return (
+            <li
+              key={"relativeListItem" + (parseInt(idx) + 1)}
+              id={"relativeListItem" + (parseInt(idx) + 1)}
+            >
+              {relative}
+            </li>
+          );
+        })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
